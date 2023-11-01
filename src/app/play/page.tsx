56471351f2a0ac747a89049a3e694abb371ac106
Spookyless <spookyless.info@gmail.com>
2023-11-01 16:20:37 +0100

    Fixed Apollo Server + websocket + NextJS intergration

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,10 +1,14 @@
 'use client'
 import { ApolloSandbox } from '@apollo/sandbox/react';
   
+import "./page.css";
+
 export default function EmbeddedSandbox() {
   return (
-    <ApolloSandbox
-      initialEndpoint='http://localhost:3000'
-    />
+    <div id='sandbox-container'>
+      <ApolloSandbox
+        initialEndpoint='http://localhost:3000/api/graphql'
+      />
+    </div>
   );
 }
